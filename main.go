89da52b1abb6ed3171d6e391124d345f5680a84c
royalblue <glwingstne@gmail.com>
2023-11-16 21:03:03 +0300

    проверка метода

--- main.go
+++ main.go
@@ -8,28 +8,35 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		w.Write([]byte("page not found"))
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Page not found"))
 		return
 	}
-	w.Write([]byte("домашняя страница"))
+	w.Write([]byte("Список фильмов"))
 }
 func secondPage(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("вторая страница"))
-}
-func thirdPage(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil && id < 1 {
-		w.Write([]byte("page not found"))
+		w.Write([]byte("Movie not found"))
 		return
 	}
-	w.Write([]byte("третья страница"))
+	w.Write([]byte("Детали фильма"))
+}
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("GET method not allowed"))
+		return
+	}
+	w.Write([]byte("Добавление нового фильма"))
 }
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/pagetwo", secondPage)
-	mux.HandleFunc("/pagethree", thirdPage)
-	log.Println("запуск")
+	mux.HandleFunc("/movie", secondPage)
+	mux.HandleFunc("/movie/createMovie", createMovie)
+	log.Println("Запуск веб-сервиса")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
